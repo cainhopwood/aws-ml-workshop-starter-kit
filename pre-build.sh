014519476d36c54120cc7d95469abaec4619dd79
Cain Hopwood <cainhopw@amazon.com>
2020-09-02 17:02:34 +1000

    Added CodeBuild spec file

--- pre-build.sh
+++ pre-build.sh
@@ -1,2 +1,5 @@
 #!/bn/bash
-aws s3 ls
+# potentially handy commands
+# aws s3 cp static/yaml/Workshop-test.yml s3://cvhxp-aiml-workshop-cftemplates
+# aws s3api put-object-acl --bucket cvhxp-aiml-workshop-cftemplates --key Workshop-test.yml --acl public-read
+aws s3 ls 
