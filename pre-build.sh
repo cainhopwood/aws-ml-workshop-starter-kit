5fbed7f4d5c1ca6fa5440ccacb052c1b31188a91
Cain Hopwood <cainhopw@amazon.com>
2020-11-02 17:33:19 +1100

    Adjust pre-build script to pull CF templates from publlic prod URL

--- pre-build.sh
+++ pre-build.sh
@@ -8,14 +8,17 @@
 # $1 is the S3 bucket prefix where the yml files in static/yaml will be deployed (does not need to be public)
 #  Note, do not include trailing slash e.g. s3://cvhxp-aimlws-build-artifacts/yaml
 
+# Changes for prod, just assume all the URLs run from the hard coded prod URL. (should be a build var)
+
 shopt -s nullglob
 echo "#!/bin/bash" > buildhugo.sh
 cd static/yaml
 for f in *
 do
     nodash=${f//-/_}
-    echo "aws s3 presign $1/$f --expires-in 604800"
-    psurl=`aws s3 presign $1/$f --expires-in 604800`
+    #echo "aws s3 presign $1/$f --expires-in 604800"
+    #psurl=`aws s3 presign $1/$f --expires-in 604800`
+    psurl="https://virtual-aiml-loft-site.s3-ap-southeast-2.amazonaws.com/yaml/$f"
     echo "export ${nodash//./_}=\"$psurl\"" >> ../../buildhugo.sh
 done
 echo "hugo -v" >> ../../buildhugo.sh
