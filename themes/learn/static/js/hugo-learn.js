eb3b44512912a1f0e5157e555f77468d669058a1
grr <grr@amazon.com>
2020-11-09 04:15:20 +0000

    making survey smaller

--- themes/learn/static/js/hugo-learn.js
+++ themes/learn/static/js/hugo-learn.js
@@ -32,6 +32,9 @@ images.wrap(function(){
 // Change styles, depending on parameters set to the image
 images.each(function(index){
   var image = $(this)
+  var width = image.width;
+  var height = image.height;
+  var cssclass = image.class;
   var o = getUrlParameter(image[0].src);
   if (typeof o !== "undefined") {
     var h = o["height"];
@@ -40,18 +43,18 @@ images.each(function(index){
     image.css("width", function() {
       if (typeof w !== "undefined") {
         return w;
-      } else {
+      } else if (typeof width === "undefined" ) {
         return "auto";
       }
     });
     image.css("height", function() {
       if (typeof h !== "undefined") {
         return h;
-      } else {
+      } else if (typeof height === "undefined" ) {
         return "auto";
       }
     });
-    if (typeof c !== "undefined") {
+    if (typeof c !== "undefined" && typeof cssclass !== "undefined") {
       var classes = c.split(',');
       for (i = 0; i < classes.length; i++) {
         image.addClass(classes[i]);
